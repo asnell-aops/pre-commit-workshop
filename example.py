3383f172d5e7e037c8ff68154a54ea792265474b
asnell-aops <asnell@aops.com>
2025-05-15 18:51:58 +0000

    add docstrings

--- example.py
+++ example.py
@@ -1,3 +1,13 @@
+"""An example file. It's for testing."""
+
+
 def my_function(a):
-    """My function."""
+    """
+    My function.
+
+    Parameters
+    ----------
+    a : anything
+        An unused parameter.
+    """
     pass
